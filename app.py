be74b27e3fa9efdab2d597c42a563941c618ed2b
seunome <seuemail@gmail.com>
2026-05-21 11:33:16 -0300

    corrige secret key

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.secret_key = "minha_chave_secreta_123"
+app.secret_key = "123456"
 
 
 def init_db():
